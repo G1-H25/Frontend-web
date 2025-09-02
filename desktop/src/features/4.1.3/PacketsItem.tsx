2424bd7ac1e05009dd3bff6cb2fa81e6920852c1
ToeCrow <thomas.kronvold@chasacademy.se>
2025-09-02 18:11:28 +0200

    added hover on PacketsItem

--- desktop/src/features/4.1.3/PacketsItem.tsx
+++ desktop/src/features/4.1.3/PacketsItem.tsx
@@ -9,14 +9,15 @@ type PacketProps = {
 
 const PacketsItem = ({ bil, chauffor, paketnr, temp, hamtat, lamnat }: PacketProps) => {
   return (
-    <tr className="bg-[#D9F2FF]">
-      <td className="text-[#2782E2] text-center p-2">{bil}</td>
-      <td className="text-[#2782E2] text-center p-2">{chauffor}</td>
-      <td className="text-[#2782E2] text-center p-2">{paketnr}</td>
-      <td className="text-[#2782E2] text-center p-2">{temp}</td>
-      <td className="text-[#2782E2] text-center p-2">{hamtat}</td>
-      <td className="text-[#2782E2] text-center p-2">{lamnat}</td>
-    </tr>
+    <tr className="bg-[#D9F2FF] text-[#2782E2] text-center hover:bg-[#2782E2] hover:text-white transition-colors">
+  <td className="p-2">{bil}</td>
+  <td className="p-2">{chauffor}</td>
+  <td className="p-2">{paketnr}</td>
+  <td className="p-2">{temp}</td>
+  <td className="p-2">{hamtat}</td>
+  <td className="p-2">{lamnat}</td>
+</tr>
+
   )
 }
 
