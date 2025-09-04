99ce21de6b475c75b3dafd6cf3ee7f5aaed979b3
ToeCrow <thomas.kronvold@chasacademy.se>
2025-09-04 23:42:25 +0200

    uppdaterat ci och package.json för att passa där vi är just nu

--- desktop/src/features/4.1.3/PacketsItem.tsx
+++ desktop/src/features/4.1.3/PacketsItem.tsx
@@ -10,13 +10,13 @@ type PacketProps = {
 const PacketsItem = ({ bil, chauffor, paketnr, temp, hamtat, lamnat }: PacketProps) => {
   return (
     <tr className="table-row table-row-hover">
-  <td className="p-2">{bil}</td>
-  <td className="p-2">{chauffor}</td>
-  <td className="p-2">{paketnr}</td>
-  <td className="p-2">{temp}</td>
-  <td className="p-2">{hamtat}</td>
-  <td className="p-2">{lamnat}</td>
-</tr>
+      <td className="p-2">{bil}</td>
+      <td className="p-2">{chauffor}</td>
+      <td className="p-2">{paketnr}</td>
+      <td className="p-2">{temp}</td>
+      <td className="p-2">{hamtat}</td>
+      <td className="p-2">{lamnat}</td>
+    </tr>
 
   )
 }
