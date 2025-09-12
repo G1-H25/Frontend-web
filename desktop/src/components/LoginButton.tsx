34c317f53afba1deae691c28ac0551d4457351a7
ToeCrow <thomas.kronvold@chasacademy.se>
2025-09-12 17:31:02 +0200

    just playing with the layout of the site

--- desktop/src/components/LoginButton.tsx
+++ desktop/src/components/LoginButton.tsx
@@ -1,6 +1,6 @@
 const LoginButton = () => {
   return (
-    <button className="bg-white text-[#2782E2] px-4 py-2 rounded-md font-medium hover:bg-[#D9F2FF] transition">
+    <button className="bg-[#D01338] text-white px-4 py-2 rounded-md font-medium hover:bg-[#A91330] transition">
       Logga in
     </button>
   )
