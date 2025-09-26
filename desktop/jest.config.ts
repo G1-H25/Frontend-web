3a343db2393b2cfe446f7400d8cdf504e8315323
ToeCrow <thomas.kronvold@chasacademy.se>
2025-09-26 12:59:59 +0200

    ...

--- desktop/jest.config.ts
+++ desktop/jest.config.ts
@@ -1,18 +1,18 @@
 import type { JestConfigWithTsJest } from 'ts-jest';
 
 const config: JestConfigWithTsJest = {
-  preset: 'ts-jest/presets/default-esm',  // <-- ESM preset
+  preset: 'ts-jest/presets/js-with-ts',
   testEnvironment: 'jsdom',
-  extensionsToTreatAsEsm: ['.ts', '.tsx'], // <-- markera TS/TSX som ESM
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   testMatch: ['**/*.test.(ts|tsx)'],
   setupFilesAfterEnv: ['@testing-library/jest-dom'],
   globals: {
     'ts-jest': {
-      tsconfig: './tsconfig.app.json',
-      useESM: true,  // <-- aktiverar ESM-stöd i ts-jest
-    },
-  },
+      tsconfig: './tsconfig.jest.json', // <- pekar på den nya
+      useESM: true
+    }
+  }
 };
 
 export default config;
