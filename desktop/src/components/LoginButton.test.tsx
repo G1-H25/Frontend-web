1601548cfcd7aeb2b1619b7cfd29109ae5c7c4a9
ToeCrow <thomas.kronvold@chasacademy.se>
2025-10-07 14:31:53 +0200

    adjustment to testfile

--- desktop/src/components/LoginButton.test.tsx
+++ desktop/src/components/LoginButton.test.tsx
@@ -1,6 +1,9 @@
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import LoginButton from "./LoginButton";
+import { TextEncoder, TextDecoder } from "util";
+(global as any).TextEncoder = TextEncoder;
+(global as any).TextDecoder = TextDecoder;
 
 describe("LoginButton", () => {
   it("should render a button with the text 'Logga in'", () => {
