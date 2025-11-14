5ad7881f14ad9856e1e695ad59e3612dcf017025
ToeCrow <thomas.kronvold@chasacademy.se>
2025-11-14 16:56:41 +0100

    https:

--- desktop/src/config/api.ts
+++ desktop/src/config/api.ts
@@ -3,4 +3,4 @@
 export const API_URL =
   import.meta.env.DEV
     ? "/api" // Vite proxy i utveckling
-    : "http://trackapp-api-env.eba-cjwxp2te.eu-north-1.elasticbeanstalk.com"; // RIKTIGA API:et i produktion
+    : "https://trackapp-api-env.eba-cjwxp2te.eu-north-1.elasticbeanstalk.com"; // RIKTIGA API:et i produktion
