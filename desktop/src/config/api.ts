516b0cdc4b76053be2ed0046a78978ec1da3cbdb
ToeCrow <thokro77@gmail.com>
2025-11-17 14:06:52 +0100

    back to http

--- desktop/src/config/api.ts
+++ desktop/src/config/api.ts
@@ -3,4 +3,4 @@
 export const API_URL =
   import.meta.env.DEV
     ? "/api" // Vite proxy i utveckling
-    : "https://trackapp-api-env.eba-cjwxp2te.eu-north-1.elasticbeanstalk.com"; // RIKTIGA API:et i produktion
+    : "http://trackapp-api-env.eba-cjwxp2te.eu-north-1.elasticbeanstalk.com"; // RIKTIGA API:et i produktion
